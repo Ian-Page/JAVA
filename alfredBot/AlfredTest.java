df8028e96204ea77005411b55ff652d201739c40
Ian Page <ianpageproductions@gmail.com>
2022-06-06 21:50:49 -0700

    perfection

--- alfredBot/AlfredTest.java
+++ alfredBot/AlfredTest.java
@@ -11,10 +11,11 @@ public class AlfredTest {
         String testDateAnnouncement = alfredBot.dateAnnouncement();
         
         String alexisTest = alfredBot.respondBeforeAlexis(
-                            "Alexis Play some low-fi beats."
-                            );
+            "Alexis Play some low-fi beats."
+        );
         String alfredTest = alfredBot.respondBeforeAlexis(
-            "I can't find my yo-yo. Maybe Alfred will know where it is.");
+            "I can't find my yo-yo. Maybe Alfred will know where it is."
+        );
         String notRelevantTest = alfredBot.respondBeforeAlexis(
             "Maybe that's what Batman is about. Not winning. But failing.."
         );
